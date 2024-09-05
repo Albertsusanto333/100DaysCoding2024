bdd2ff4a7751909b33bb612c8ad94354b1b58bc5
Albertsusanto333 <albertsusanto394@gmail.com>
2024-09-05 17:52:56 +0800

    Hari 4
    Menampilkan nama, nim, umur, tinggi badan

---
+++ Day4.java
@@ -0,0 +1,8 @@
+package Day4;
+
+public class Day4 {
+    
+    public static void main(String[] args){
+        System.out.printf("nama = %s,nim = %s,umur = %d,tinggi badan = %f","Albert susanto","D0224513",19,168.5);
+    }
+}
