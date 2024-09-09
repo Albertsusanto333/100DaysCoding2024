fae26b0e7449941ff5f46c3cf90ad3805a292c36
Albertsusanto333 <albertsusanto394@gmail.com>
2024-09-09 19:06:19 +0800

    Hari 8
    Membuat variabel update

---
+++ Day8.java
@@ -0,0 +1,13 @@
+package Day8;
+
+public class Day8 {
+    public static void main(String[] args) {
+        int umur = 19;
+        //mendeklarasikan dan menginisialisasi variabel
+        System.out.println("Nilai awal: " + umur);
+        umur = 19;
+        //memperbarui nilai variabel
+        
+    
+    } 
+}
