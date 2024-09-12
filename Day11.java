66ebc4b02a803058909eb176fb5dc12f94ab0ce0
Albertsusanto333 <albertsusanto394@gmail.com>
2024-09-12 16:52:00 +0800

    Hari 11
    Menampilkan tipe data : short

---
+++ Day11.java
@@ -0,0 +1,9 @@
+package Day11;
+
+public class Day11 {
+    public static void main(String[] args){
+    short bilangan = 22;
+    System.out.println(bilangan);
+        //menampilkan tipe data : short
+}
+}
