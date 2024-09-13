2eea562e88ebe5b5911c35af505e5589862884cc
Albertsusanto333 <albertsusanto394@gmail.com>
2024-09-13 17:55:55 +0800

    Hari 12
    Menampilkan tipe data : integer

---
+++ Day12.java
@@ -0,0 +1,9 @@
+package Day12;
+
+public class Day12 {
+    public static void main(String[] args){
+    int umur = 19;
+        System.out.println(umur);
+        //menampilkan tipe data : int
+    }
+}
