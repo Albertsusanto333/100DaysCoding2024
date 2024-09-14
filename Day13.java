3eff6fb52f967bbd86190701d6f8387485c94440
Albertsusanto333 <albertsusanto394@gmail.com>
2024-09-14 11:01:54 +0800

    Hari 13
    Latihan(evaluasi)

---
+++ Day13.java
@@ -0,0 +1,11 @@
+package Day13;
+
+public class Day13 {
+    public static void main(String[] args){
+    byte a;
+    a = 3;
+        System.out.println(a);
+        a = 4;
+        System.out.println(a);
+    }
+}
