87ce64e04b51e846c6bed05f425609b359900847
Albertsusanto333 <albertsusanto394@gmail.com>
2024-09-15 20:24:17 +0800

    Hari 14
    Menampilkan tipe data : long

---
+++ Day14.java
@@ -0,0 +1,10 @@
+package Day14;
+
+public class Day14 {
+    public static void main(String[] args){
+        long bilangan = 123456789L;
+        System.out.println(bilangan);
+    }
+    
+    
+}
