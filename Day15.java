7b5507bc632e675f8bfcac92e4585f0eeaf650d6
Albertsusanto333 <albertsusanto394@gmail.com>
2024-09-16 21:00:32 +0800

    Hari 15
    Menampilkan tipe data : double

---
+++ Day15.java
@@ -0,0 +1,10 @@
+package Day15;
+
+public class Day15 {
+    public static void main(String[] args){
+        double berat = 60.5;
+        System.out.println(berat);
+        //menampilkan tipe data double berat dengan nilai 60.5
+    }
+    
+}
