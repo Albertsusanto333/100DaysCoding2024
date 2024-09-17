0cc96b099847a5a066224d3f78d9761e6bda8345
Albertsusanto333 <albertsusanto394@gmail.com>
2024-09-18 07:18:43 +0800

    Hari 17
    Menampilkan tipe data char (simbol = $)

---
+++ Day17.java
@@ -0,0 +1,10 @@
+package Day16;
+
+public class Day16 {
+    public static void main(String[] args){
+        float nilai = 1.23f;
+        System.out.println(nilai);
+        //menampilkan tipe data float nilai = 1.23
+    }
+    
+}
